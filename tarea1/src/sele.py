f380cdcd4cca72896c5774a184677991a6670e5b
William <wil-20-01@live.com>
2023-01-30 19:36:30 -0600

    Incluir tarea 2 y renombrar archivos

--- tarea1/src/sele.py
+++ tarea1/src/sele.py
@@ -2,7 +2,7 @@ import pandas as pd
 import yaml 
 import os
 
-config_path = os.path.join("/home/william/claseJimmy/config", "config.yml" )
+config_path = os.path.join("/home/william/claseJimmy/tarea1/config", "config.yml" )
 
 config = yaml.load(open(config_path), Loader=yaml.FullLoader)
 
