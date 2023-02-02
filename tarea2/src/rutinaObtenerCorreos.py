add9a6ce2d2e69e8f3ee5aff55c07f1e209b3f84
William <wil-20-01@live.com>
2023-02-01 18:51:11 -0600

    orden de codigo

--- tarea2/src/rutinaObtenerCorreos.py
+++ tarea2/src/rutinaObtenerCorreos.py
@@ -3,17 +3,13 @@ import yaml
 import os 
 
 config_path = os.path.join("/home","william","claseJimmy","tarea2","config", "config.yml")
-
 config = yaml.load(open(config_path), Loader=yaml.FullLoader)
 
 io = open(os.path.join(config['file_path']), 'r')
-
 texto = io.read()
 correos = re.findall(config['regex'], texto)
 io.close()
 
 print("Cantidad de correos", len(correos))
-
-
 print(correos)
     
