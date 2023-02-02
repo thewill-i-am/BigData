269760ec54fc528b44a3880a88f2d5e52f39d35a
William <wil-20-01@live.com>
2023-02-01 18:44:30 -0600

    fixing path issues

--- tarea2/src/rutinaObtenerCorreos.py
+++ tarea2/src/rutinaObtenerCorreos.py
@@ -2,10 +2,11 @@ import re
 import yaml
 import os 
 
-config_path = os.path.join("/home/william/claseJimmy/tarea2/config", "config.yml")
+config_path = os.path.join("/home","william","claseJimmy","tarea2","config", "config.yml")
+
 config = yaml.load(open(config_path), Loader=yaml.FullLoader)
 
-io = open(os.path.join("/home/william/claseJimmy/tarea2", config['file_path']), 'r')
+io = open(os.path.join(config['file_path']), 'r')
 
 texto = io.read()
 correos = re.findall(config['regex'], texto)
